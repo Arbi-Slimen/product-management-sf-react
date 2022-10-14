83dd1b9453e035325ba8510cb0ec14800feec6d8
arbi <arbislimen@gmail.com>
2022-10-14 11:44:24 +0100

    add Footer.js

---
+++ frontend-react/src/component/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer className="py-5 bg-dark">
+            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; ATS DIGITAL 2022</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
