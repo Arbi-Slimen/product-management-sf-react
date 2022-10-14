430b18daa520a87843918bb8fc9b8c175b4c2706
arbi <arbislimen@gmail.com>
2022-10-14 10:37:31 +0100

    Install and configure nelmio/cors-bundle

--- Backend-Symfony/config/bundles.php
+++ Backend-Symfony/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
