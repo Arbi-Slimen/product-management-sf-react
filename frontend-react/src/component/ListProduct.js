b4895f4498385bdb5334746e9945a87226a7889c
arbi <arbislimen@gmail.com>
2022-10-16 15:56:29 +0100

    update ListProduct.js

--- frontend-react/src/component/ListProduct.js
+++ frontend-react/src/component/ListProduct.js
@@ -1,5 +1,4 @@
-import Cart from "./Product";
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import Page from "./Page";
 import {Loader} from "./Loader";
 import {useFetch} from "../api/api";
