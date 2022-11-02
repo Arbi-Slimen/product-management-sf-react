a34bc5b2de2163b1f367e64bb238bfd5547676cf
Arbi Slimen <115590103+Arbi-Slimen@users.noreply.github.com>
2022-11-02 10:28:21 +0100

    Update Footer.js

--- frontend-react/src/component/Footer.js
+++ frontend-react/src/component/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="py-5 bg-dark">
-            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; ATS DIGITAL 2022</p>
+            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; 2022</p>
             </div>
         </footer>
     )
